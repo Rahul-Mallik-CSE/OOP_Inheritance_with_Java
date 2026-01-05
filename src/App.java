bbed52883d8098d39019f98d2542f862a4161735
Rahul Mallik <rmallik191242@bscse.uiu.ac.bd>
2026-01-05 23:26:48 +0600

    Add Parent class with a method and variable for inheritance demonstration

--- src/App.java
+++ src/App.java
@@ -1,3 +1,14 @@
+
+class Parent {
+    int parentVariable = 10;
+
+    public void parentMethod() {
+        System.out.println("This is a method from the Parent class.");
+    }
+}
+
+
+
 class Inheritance {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
